a50a153901a40c2aec5b153a6eed46265f82a655
Teddy <tedyzetaa@gmail.com>
2025-11-20 07:30:39 -0400

    1.0.5

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const socketIo = require('socket.io');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const connectDB = require('../config/database');
+const connectDB = require('./config/database');
 
 const app = express();
 const server = http.createServer(app);
